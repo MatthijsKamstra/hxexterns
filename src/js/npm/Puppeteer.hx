cc5ca54a0f31e853c3b26b6770ae9a1b9f300913
Matthijs Kamstra <matthijs@fonk.amsterdam>
2018-01-01 13:40:20 +0100

    euhhhh puppeteer uses async await... need some macro magic to make that work

---
+++ src/js/npm/Puppeteer.hx
@@ -0,0 +1,7 @@
+package js.npm;
+
+@:native('puppeteer')
+@:jsRequire("puppeteer")
+extern class Poppeteer {
+
+}
---
+++ test/MainPuppeteer.hx
@@ -0,0 +1,20 @@
+package;
+
+
+import js.npm.Puppeteer;
+
+import js.Browser.*;
+import js.Browser.console;
+import js.html.Window.*;
+// import js.html.Window.setTimeout;
+
+class MainPuppeteer {
+
+	public function new() {
+
+	}
+
+	static public function main() {
+		var app = new MainPuppeteer();
+	}
+}
