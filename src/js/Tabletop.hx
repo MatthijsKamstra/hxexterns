eccba9f50d7c40ad36b3a9018c0cbf43afeef8be
Matthijs Kamstra <matthijs@fonk.amsterdam>
2018-02-17 11:59:53 +0100

    minor update to the callback, added some documentation

--- src/js/Tabletop.hx
+++ src/js/Tabletop.hx
@@ -1,13 +1,14 @@
 package js;
 
 import haxe.extern.EitherType;
+import haxe.Constraints.Function;
 
 typedef TabletopObj = {
 
 	var key : String; // 'https://docs.google.com/spreadsheets/d/0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE/pubhtml',
 
 
-	@:optional var callback : Dynamic; 		// function(data, tabletop) {
+	@:optional var callback : String -> Dynamic -> Void ; 		// function(data, tabletop) {
 	@:optional var postProcess : Dynamic;
 	@:optional var callbackContext : Dynamic;
 	// @:optional var parameterize : Dynamic;
@@ -35,16 +36,44 @@ typedef TabletopObj = {
 
 }
 
+typedef TabletopModelObj = {
+	var columnNames : Array<String>;
+	var column_names : Array<String>;
+	var elements : Array<Dynamic>;
+	var name : String;
+	var onReady : Function ;
+	var originalColumns : Array<String>;
+	var original_columns : Array<String>;
+	var prettyColumns : Dynamic;
+	var pretty_columns : Dynamic;
+	var raw : Dynamic;
+}
+
+/**
+ * name: tabletop
+ * version: 1.5.3
+ * description: Tabletop.js takes a Google Spreadsheet and makes it easily accessible through JavaScript. With zero dependencies!
+ *
+ * url: https://github.com/jsoma/tabletop
+ */
 @:native('Tabletop')
 extern class Tabletop {
-	public function new() {
+
+	public function new() {}
+
+	public static function init(tabletopObj:TabletopObj):Void {};
 
 
-
-	}
-
-	public static function init(tabletopObj:TabletopObj):Void {
-	};
+	// public function sheets(name:String) : TabletopModelObj;
+	// public function modelNames():Void;
+	// public function foundSheetNames():Void;
+	// public function data():Void;
+	// public function fetch():Void;
+	// public function addWanted(name):Void;
 
 
 }
+
+// class TabletopModel {
+
+// }
