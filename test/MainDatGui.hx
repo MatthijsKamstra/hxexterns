141be97362ad9190d2cf28a5e98c6b4a2005338f
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-07-07 21:12:39 +0200

    cleaner example of dat.GUI with haxe

--- test/MainDatGui.hx
+++ test/MainDatGui.hx
@@ -1,71 +1,58 @@
 class MainDatGui {
 
-	var message:String;
-	var speed:Float;
-	var displayOutline:Bool;
-	var explode:Dynamic;
-	var noiseStrength:Float;
-
-	var growthSpeed:Float;
-	var maxSize:Float;
-
-
-	public function new() {
-		init0();
-	}
-
-	function FizzyText ():Dynamic {
-		this.message = 'dat.gui';
-		this.speed = 0.8;
-		this.displayOutline = false;
-		this.noiseStrength = 0.8;
-		this.growthSpeed = 0.8;
-		this.maxSize = 0.8;
-		this.explode = function() {
+	var message = 'dat.gui';
+	var speed = 0.8;
+	var displayOutline = false;
+	var noiseStrength = 0.8;
+	var growthSpeed = 0.8;
+	var maxSize = 0.8;
+	var explode = function() {
 			trace('booom');
 		};
 
-		js.Lib.nativeThis.color0 = "#ffae23"; // CSS string
-		js.Lib.nativeThis.color1 = [ 0, 128, 255 ]; // RGB array
-		js.Lib.nativeThis.color2 = [ 0, 128, 255, 0.3 ]; // RGB with alpha
-		js.Lib.nativeThis.color3 = { h: 350, s: 0.9, v: 0.3 }; // Hue, saturation, value
+	var color0 = "#ffae23"; // CSS string
+	var color1 = [ 0, 128, 255 ]; // RGB array
+	var color2 = [ 0, 128, 255, 0.3 ]; // RGB with alpha
+	var color3 = { h: 350, s: 0.9, v: 0.3 }; // Hue, saturation, value
 
-		return {
-			message:this.message,
-			speed:this.speed,
-			maxSize:this.maxSize,
-			noiseStrength:this.noiseStrength,
-			growthSpeed:this.growthSpeed,
-			displayOutline:this.displayOutline,
-			explode:this.explode
-		};
-	};
+	public function new() {
+		init0();
+		init1();
+	}
+
+	function init1(){
+		var gui = new js.dat.GUIX();
+		gui.add(this, 'message');
+		// gui.add(this, 'speed', -5, 5);
+		gui.add(this, 'displayOutline');
+		gui.add(this, 'explode');
+		gui.add(this, 'fail');
+
+	}
 
 	function init0(){
-		var text = FizzyText();
-
 		var gui = new js.dat.gui.GUI();
-		gui.add(text, 'message');
-		gui.add(text, 'speed', -5, 5);
-		gui.add(text, 'displayOutline');
-		gui.add(text, 'explode');
+		gui.add(this, 'message');
+		gui.add(this, 'speed', -5, 5);
+		gui.add(this, 'displayOutline');
+		gui.add(this, 'explode');
 
 		var f1 = gui.addFolder('Flow Field');
-		f1.add(text, 'speed');
-		f1.add(text, 'noiseStrength');
+		f1.add(this, 'speed');
+		f1.add(this, 'noiseStrength');
 
 		var f2 = gui.addFolder('Letters');
-		f2.add(text, 'growthSpeed');
-		f2.add(text, 'maxSize');
-		f2.add(text, 'message');
+		f2.add(this, 'growthSpeed');
+		f2.add(this, 'maxSize');
+		f2.add(this, 'message');
 
 
-		// gui.addColor(text, 'color0');
-		// gui.addColor(text, 'color1');
-		// gui.addColor(text, 'color2');
-		// gui.addColor(text, 'color3');
+		gui.addColor(this, 'color0');
+		gui.addColor(this, 'color1');
+		gui.addColor(this, 'color2');
+		gui.addColor(this, 'color3');
 
-		var controller = gui.add(text, 'maxSize', 0, 10);
+		var controller = gui.add(this, 'maxSize', 0, 10);
 		controller.onChange(function(value) {
 			// Fires on every change, drag, keypress, etc.
 			trace('value: $value');
