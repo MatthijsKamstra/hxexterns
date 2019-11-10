9c325f429ccadd1e9daffa2aaf276056afc9b10f
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-11-10 12:42:27 +0100

    use promise

--- src/js/html/MediaDevices.hx
+++ src/js/html/MediaDevices.hx
@@ -1,9 +1,7 @@
 package js.html;
 
 // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-
 @:native("MediaDevices")
-extern class MediaDevices
-{
-	public function getUserMedia(constraints:Dynamic) : Promise<Dynamic>;
+extern class MediaDevices {
+	public function getUserMedia(constraints:Dynamic):js.lib.Promise<Dynamic>;
 }
