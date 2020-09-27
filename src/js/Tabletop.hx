d2d31407365de1eeec076aff7a1fceb2d25a0029
Matthijs Kamstra <matthijs@fonk.amsterdam>
2020-09-27 10:37:38 +0200

    formatting?

--- src/js/Tabletop.hx
+++ src/js/Tabletop.hx
@@ -4,49 +4,42 @@ import haxe.extern.EitherType;
 import haxe.Constraints.Function;
 
 typedef TabletopObj = {
+	var key:String; // 'https://docs.google.com/spreadsheets/d/0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE/pubhtml',
 
-	var key : String; // 'https://docs.google.com/spreadsheets/d/0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE/pubhtml',
-
-
-	@:optional var callback : String -> Dynamic -> Void ; 		// function(data, tabletop) {
-	@:optional var postProcess : Dynamic;
-	@:optional var callbackContext : Dynamic;
+	@:optional var callback:String->Dynamic->Void; // function(data, tabletop) {
+	@:optional var postProcess:Dynamic;
+	@:optional var callbackContext:Dynamic;
 	// @:optional var parameterize : Dynamic;
-
-	@:optional var simpleSheet : Bool;
-	@:optional var parseNumbers : Bool;
-	@:optional var reverse : Bool;
-	@:optional var singleton : Bool; // Defaults to false.
-	@:optional var simple_url : Bool; // Defaults to false.
-	@:optional var prettyColumnNames : Bool; // Defaults to true.
-
-	@:optional var orderby : String;
-	@:optional var wanted : String; // Defaults to all.
-	@:optional var endpoint : String;
-	@:optional var authkey : String;
-
+	@:optional var simpleSheet:Bool;
+	@:optional var parseNumbers:Bool;
+	@:optional var reverse:Bool;
+	@:optional var singleton:Bool; // Defaults to false.
+	@:optional var simple_url:Bool; // Defaults to false.
+	@:optional var prettyColumnNames:Bool; // Defaults to true.
+	@:optional var orderby:String;
+	@:optional var wanted:String; // Defaults to all.
+	@:optional var endpoint:String;
+	@:optional var authkey:String;
 	// @:optional var debug : String;
 	// @:optional var wait : String;
-
 	/**
 	 *  callback:String -> Null<Dynamic> -> Void
 	 *
 	 *
 	 */
-
 }
 
 typedef TabletopModelObj = {
-	var columnNames : Array<String>;
-	var column_names : Array<String>;
-	var elements : Array<Dynamic>;
-	var name : String;
-	var onReady : Function ;
-	var originalColumns : Array<String>;
-	var original_columns : Array<String>;
-	var prettyColumns : Dynamic;
-	var pretty_columns : Dynamic;
-	var raw : Dynamic;
+	var columnNames:Array<String>;
+	var column_names:Array<String>;
+	var elements:Array<Dynamic>;
+	var name:String;
+	var onReady:Function;
+	var originalColumns:Array<String>;
+	var original_columns:Array<String>;
+	var prettyColumns:Dynamic;
+	var pretty_columns:Dynamic;
+	var raw:Dynamic;
 }
 
 /**
@@ -58,22 +51,15 @@ typedef TabletopModelObj = {
  */
 @:native('Tabletop')
 extern class Tabletop {
-
-	public function new() {}
-
-	public static function init(tabletopObj:TabletopObj):Void {};
-
-
+	// public function new() {}
+	public static function init(tabletopObj:TabletopObj):Void;
 	// public function sheets(name:String) : TabletopModelObj;
 	// public function modelNames():Void;
 	// public function foundSheetNames():Void;
 	// public function data():Void;
 	// public function fetch():Void;
 	// public function addWanted(name):Void;
-
-
 }
 
 // class TabletopModel {
-
 // }
